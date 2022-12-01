17b28c9d2d254a36b8f7246610854af9607e8d96
Rodrigo Carrillo-Sanchez <rodrigo.carrillo-sanchez@dartmouth.edu>
2022-12-01 11:00:02 -0500

    improve naming convention

---
+++ day01.py
@@ -0,0 +1,22 @@
+def getCaloriesPerElf():
+    elves = []
+    
+    with open('day1input.txt') as source:
+        tempArray = []
+        for row in source:
+            if len(row) > 1:
+                tempArray.append(int(row.strip()))
+            else:
+                elves.append(tempArray)
+                tempArray = []
+    
+    return map(lambda i: sum(i), elves)
+    
+def part1():
+    return max(getCaloriesPerElf())
+
+def part2():
+    return sum(sorted(getCaloriesPerElf())[-3:])
+
+
+print(part1(), part2())
